7fd1a9fa6978b184e70cc2de8c9537d17e883938
Trevor Seitz <trevinla@gmail.com>
2018-04-10 17:13:03 -0400

    menu system is working - UFO news is working

--- lib/articles.rb
+++ lib/articles.rb
@@ -5,40 +5,24 @@ class Article
   @@all = []
 
   def initialize(article_title, article_info, article_text, article_url, comments_url)
-# binding.pry
     @article_title = article_title
-    @article_date = article_info[0]
+    @article_date = article_info
     @article_text = article_text
     @article_url = article_url
     @comments_url = comments_url
     @@all << self
-    # display_summary
-    binding.pry
   end
 
   def self.all
     @@all
   end
 
-  # def self.create_from_collection(article_array)
-  #   article_array.each do |article|
-  #     self.new(article)
-  #   end
-  # end
-
-  # def display_summary
-  #   counter = 0
-  #   binding.pry
-  #   self.all.each do |article|
-  #     counter += 1
-  #     puts "#{counter}. #{@article_title}  -  #{@article_date}"
-  #   end
-  # end
-
-
-
-
-  # def display_news
-
+  def display_summary
+    counter = 0
+    self.all.each do |article|
+      counter += 1
+      puts "#{counter}. #{@article_title}  -  #{@article_date}"
+    end
+  end
 
 end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -9,32 +9,36 @@ class CommandLineInterface
     menu
   end
 
+  def wrap(s, width=125)
+	  s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+	end
+
   def menu
     user_input = ""
 
-    while while user_input != "exit"
+    while user_input != "exit"
+      puts `clear`
       puts "Welcome to the MUFON News Scraper!"
       puts "What would you like to do?"
       puts "1. UFO news"
       puts "2. MUFON news"
       puts "3. List of the last 20 UFO Reports"
-      puts "type exit to exit"
+      puts "type 'exit' to exit"
 
       user_input = gets.chomp.downcase
 
       case user_input
       when "1"
         Scraper.new(BASE_PATH)
-        puts ""
-        puts "Welcome to MUFON's UFO news"
-        puts ""
         display_summary
       when "2"
         break
       end
     end
+    puts `clear`
+    abort
   end
-  end
+ 
 
   def run
     article_info
@@ -54,12 +58,45 @@ class CommandLineInterface
 
   def display_summary
     counter = 0
-    binding.pry
-    Article.all.each do |article|
-      counter += 1
-      binding.pry
-      puts "#{counter}. #{article.article_title}  -  #{article.article_date}"
+    continue = ""
+    user_input = ""
+
+    while user_input != "exit"
+        puts `clear`
+        puts "Welcome to MUFON's UFO news"
+        puts ""
+      Article.all.each do |article|
+        counter += 1
+        puts "#{counter}. #{article.article_title}  -  #{article.article_date}"
+      end
+      puts ""
+      puts "type exit to return to the main menu"
+      puts "or 'quit' to quit"
+    user_input = gets.chomp.downcase
+    if user_input == "exit"
+      menu
+    elsif user_input == "quit"
+      puts `clear`
+      abort
     end
+    index = user_input.to_i - 1
+    display_story(index)
+    end
+    
+  end
+
+  def display_story(index)
+    continue = ""
+    while continue != "exit"
+      puts `clear`
+      puts "#{Article.all[index].article_title}  -  #{Article.all[index].article_date}"
+      puts ""
+      puts wrap(Article.all[index].article_text.gsub("by Roger Marsh", "by Roger Marsh \n").gsub("READ MORE", ""))
+      puts ""
+      puts "type 'exit' to return to the story list"
+      continue  = gets.chomp.downcase
+    end
+    display_summary
   end
 
 end
