5d2006d2adb998d84303f344a39fd1e4e1470c50
Trevor Seitz <trevinla@gmail.com>
2018-04-13 11:33:39 -0400

    added comments to both files

--- lib/mufon_news.rb
+++ lib/mufon_news.rb
@@ -1,4 +1,5 @@
 require "mufon_news/version"
-
+# I am not sure what this file is for...  if was created with bundler
+# I am assuming it is used to launch the gem but I am not sure
 module MufonNews
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 class Scraper
 
   def initialize(site_url)
+    # Decides which scraper to use to 
     @doc = Nokogiri::HTML(open(site_url))
     if site_url.include? "news"
       parse_news(@doc)
@@ -14,24 +15,27 @@ class Scraper
   end
   
   def parse_news(nokogiri_file)
+    # Pulls the elements from the UFO news page and the MUFON news page
     nokogiri_file.css(".blog-post").each do |article|
       if article.css(".paragraph").text != ""
-        @article_junk = article.css("h2 a").text,
+        @article_junk = article.css("h2 a").text, # without this line of code - @article_title = nil
         @article_title = article.css("h2 a").text,
         @article_info = article.css(".blog-date .date-text").first.children.text.gsub("\n\t\t", "").gsub("\n\t", ""),
         @article_text = article.css(".paragraph").text,
-        @article_url = article.css(".blog-link @href").first.value, #find href for article
-        @comments_url = article.css(".blog-comments a @href").first.value.gsub("//", ""),
-        Article.new(@article_title, @article_info, @article_text, @article_url, @comments_url)
+        # @article_url = article.css(".blog-link @href").first.value, #find href for article
+        # @comments_url = article.css(".blog-comments a @href").first.value.gsub("//", ""),
+        Article.new(@article_title, @article_info, @article_text) #, @article_url, @comments_url)
       end
     end
   end
 
   def parse_report(nokogiri_file)
+    # Pulls the elements from the Recent Sightings page
     counter = 0
     nokogiri_file.css("tr").each do |row|
       counter += 1
-      if counter > 3
+      if counter > 3 # this is required to get past the 3 unwanted "tr" elements in the table
+        # there are 2 blank rows and a row of headers in the table
         @case_number = row.css("td")[0].text
         @date_submitted = row.css("td")[1].text
         @date_of_event = row.css("td")[2].text
@@ -40,9 +44,7 @@ class Scraper
         @state = row.css("td")[5].text
         @junk = row.css("td")[6].text
         Sighting.new(@case_number, @date_submitted, @date_of_event, @description, @city, @state)
-      # binding.pry
       end
     end
   end
-
 end
