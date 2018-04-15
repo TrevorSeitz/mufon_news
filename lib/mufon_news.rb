c936e98381cf197496c32808895295353dbe056f
Trevor Seitz <trevinla@gmail.com>
2018-04-15 12:18:51 -0400

    moved some require comments

--- lib/mufon_news.rb
+++ lib/mufon_news.rb
@@ -1,5 +1,6 @@
 require "mufon_news/version"
-# I am not sure what this file is for...  if was created with bundler
-# I am assuming it is used to launch the gem but I am not sure
-module MufonNews
-end
+require 'open-uri'
+require "nokogiri"
+require 'pry'
+
+
